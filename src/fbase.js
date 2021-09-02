d2a4d2ae27fcd3f06e31367e7551123b112d4288
fp024 <aopenasus@naver.com>
2021-09-03 00:45:01 +0900

    ~p93 로그인, 로그아웃하기

---
+++ src/fbase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat/app";
+/*
+  firebase/app 으로 import 하면, 아래 오류가 발생한다.
+  Attempted import error: 'firebase/app' does not contain a default export (imported as 'firebase').
+  https://stackoverflow.com/questions/68946446/how-do-i-fix-a-firebase-9-0-import-error-attempted-import-error-firebase-app
+*/
+import "firebase/compat/auth"
+/*  
+  "firebase/auth" 을 import 하면 아래 오류가 발생한다.  app과 마찬가지로 compat경로를 붙인다.
+  TypeError: firebase_compat_app__WEBPACK_IMPORTED_MODULE_0__.default.auth is not a function
+*/
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+export const authService = firebase.auth();
