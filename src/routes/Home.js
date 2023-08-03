38005681036e1046e70840c820696637d2c20f41
fp024 <aopenasus@naver.com>
2023-08-03 20:54:51 +0900

    p164 - 사진을 포함한 트윗 결과 화면에 출력하기
    - 뭔가 useEffect 관련 콘솔 오류를 봤었는데.. 재시작하고 없어진것 같아서 좀 해깔리긴한다.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -21,20 +21,22 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    /*
-    await dbService.collection('nweets').add({
-      text: nweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid
-    });
-    setNweet('');
-    */
     const attachmentRef = storageService
       .ref()
       .child(`${userObj.uid}/${uuidv4()}`);
 
     const response = await attachmentRef.putString(attachment, 'data_url');
-    console.log(response);
+    const attachmentUrl = await response.ref.getDownloadURL();
+
+    await dbService.collection('nweets').add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl
+    });
+    setNweet('');
+    setAttachment('');
+
   };
 
   const onChange = (event) => {
